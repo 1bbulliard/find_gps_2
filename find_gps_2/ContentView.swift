b0565a0a94ff0e34e45a0a5a63b1b6ccadb01ab9
bbulliard <bbulliard@gmail.com>
2022-04-01 15:36:41 -0500

    20220401_1500

--- find_gps_2/ContentView.swift
+++ find_gps_2/ContentView.swift
@@ -4,13 +4,13 @@
 //
 //  Created by Bob Bulliard on 3/31/22.
 //
-
+import Foundation
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       MapView()
     }
 }
 
@@ -19,3 +19,49 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+class LocationManager: NSObject   {
+    
+    private let locationManager = CLLocationManager()
+    var location: CLLocation? = nil
+    
+    override init() {
+        super.init()
+      //  self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLDistanceFilterNone
+        self.locationManager.distanceFilter = kCLDistanceFilterNone
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+    }
+}
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+        guard let location = locations.last else {
+            return
+        }
+        self.location = location
+    }
+}
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView  {
+       
+        let map = MKMapView()
+        map.showsUserLocation = true
+        map.delegate = context.coordinator
+        return map
+    }
+    func makeCoordinator() -> Coordinator {
+      Coordinator(self)
+    }
+    func updateUIView(_ uiView: MKMapView, context:  UIViewRepresentableContext<MapView>) {
+        
+    }
+}
+final class Coordinator: NSObject, MKMapViewDelegate {
+    var control: MapView
+    init(_ control: MapView) {
+        self.control = control
+    }
+}
+
